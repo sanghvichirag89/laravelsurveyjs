53f617d63df40ce5b7853113740cf4cee63a3339
chirag <sanghvichirag89@gmail.com>
2021-05-27 06:15:13 +0530

    remove

--- license.md
+++
@@ -1,5 +0,0 @@
-# The license
-
-Copyright (c) Aidyn Makhataev <makataev.7@gmail.com>
-
-...Add your license text here...
--- resources/assets/js/survey-front.js
+++
@@ -1,9 +0,0 @@
-require("./base")
-
-window.Vue = require("vue")
-
-Vue.component('survey-show', require('./components/SurveyShow.vue'))
-
-new Vue({
-    el: "#surveyElement"
-})
